cd0b412d5a6636c8081bd1ca00b7a21457ed3520
Yaroslav Skudarnov <skudarnovyi@gmail.com>
2017-10-22 15:00:27 +0300

    BinarySearchTree: rename abstract class

--- src/ru/yaroslavskudarnov/BST/main/AbstractBinarySearchTree.java
+++
@@ -1,6 +0,0 @@
-package ru.yaroslavskudarnov.BST.main;
-
-import java.util.AbstractCollection;
-
-abstract class AbstractBinarySearchTree<E extends Comparable<? super E>> extends AbstractCollection<E> {
-}
---
+++ src/ru/yaroslavskudarnov/BST/main/BinarySearchTree.java
@@ -0,0 +1,6 @@
+package ru.yaroslavskudarnov.BST.main;
+
+import java.util.AbstractCollection;
+
+public abstract class BinarySearchTree<E extends Comparable<? super E>> extends AbstractCollection<E> {
+}
