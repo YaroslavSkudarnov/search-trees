0819fd1c674e1807945b2a2a17740f3ed8b7d783
Yaroslav Skudarnov <skudarnovyi@gmail.com>
2018-09-12 18:40:51 +0300

    BTree: add skeleton of a BTree, implement a couple of things

---
+++ src/ru/yaroslavskudarnov/BST/main/BTree.java
@@ -0,0 +1,124 @@
+package ru.yaroslavskudarnov.BST.main;
+
+import ru.yaroslavskudarnov.BST.core.SearchTree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * User: Skudarnov Yaroslav
+ * Date: 9/12/2018
+ * Time: 3:42 PM
+ */
+public class BTree<E extends Comparable<? super E>> extends SearchTree<E> {
+    final private int MINIMUM_NUMBER_OF_KEYS_IN_A_NODE;
+    final private int NUMBER_OF_KEYS_TO_START_BINSEARCH = 5; //TODO: benchmark and set optimal value here
+    private BTreeNode root;
+
+    public BTree(int d) {
+        this.MINIMUM_NUMBER_OF_KEYS_IN_A_NODE = d;
+    }
+
+    private class BTreeNode {
+        private BTreeNode parent;
+        private List<BTreeNode> children;
+        private List<E> keys;
+
+        private BTreeNode(E payload) {
+            this.keys = new ArrayList<>();
+            keys.add(payload);
+        }
+
+        private BTreeNode(BTreeNode node) {
+            replaceContent(node);
+        }
+
+        private void replaceContent(BTreeNode replacement) {
+            this.children = new ArrayList<>(replacement.children);
+            this.keys = new ArrayList<>(replacement.keys);
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return subtreeSize(root);
+    }
+
+    private int subtreeSize(BTreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return node.children.size() + node.children.stream().map(this::subtreeSize).mapToInt(Integer::intValue).sum();
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if ((o == null) || (root == null)) {
+            return false;
+        } else {
+            @SuppressWarnings("unchecked")
+            E e = (E) o;
+
+            return subtreeContains(e, root);
+        }
+    }
+
+    private boolean subtreeContains(E e, BTreeNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        if (node.keys.size() < NUMBER_OF_KEYS_TO_START_BINSEARCH) {
+            for (int i = 0; i < node.keys.size(); ++i) {
+                int compare = node.keys.get(i).compareTo(e);
+
+                if (compare == 0) {
+                    return true;
+                } else if (compare > 0) {
+                    return subtreeContains(e, node.children.get(i));
+                }
+            }
+
+            return subtreeContains(e, node.children.get(node.keys.size()));
+        } else {
+            int l = 0, r = node.keys.size(); //TODO: test this
+
+            while (l < r) {
+                int m = (l + r) / 2;
+
+                int compare = node.keys.get(m).compareTo(e);
+
+                if (compare == 0) {
+                    return true;
+                } else if (compare > 0) {
+                    r = m;
+                } else {
+                    l = m;
+                }
+            }
+
+            return subtreeContains(e, node.children.get(l));
+        }
+    }
+}
